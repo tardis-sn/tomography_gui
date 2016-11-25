a0ec5a43ec6b6f20e631a73f0bc196de8fc8bf43
Talytha Barbosa <talytha@mpa-garching.mpg.de>
2016-11-25 16:35:53 +0100

    Find all epochs corresponding to a given Tardis run. For each of them: write and submit a job to the MPA SGE Batch System.

---
+++ tardis_multiple_runner.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import yaml
+import numpy as np
+import argparse
+import glob
+import shlex
+import subprocess
+
+def main(rootname):
+
+    naepochs = glob.glob("tardis_%05d*.yml" % rootname)
+    nepochs = len(naepochs)
+    epochs =[]
+
+    for i in xrange (nepochs):
+        epoch =(naepochs[i].rsplit("_")[-1].strip()).rsplit(".")[-2].strip()
+        print epoch
+        epochs.append(int(epoch))
+
+    return epochs
+
+def write_submit(Nthreads, filename, rootname, defaultfile = 'mpa-pascal-default.cmd'):
+
+    epochs = main(rootname)
+
+    for i in xrange(len(epochs)):
+        with open(defaultfile, 'r') as file:
+            filedata = file.read()
+
+        filedata = filedata.replace('NTHREADS', str(Nthreads))
+        filedata = filedata.replace('executablefile', './run_tardis.py')
+        filedata = filedata.replace('filename', str(filename))
+        filedata = filedata.replace('rootname', str(rootname))
+        filedata = filedata.replace('epoch', str(epochs[i]))
+
+        with open('mpa-pascal_%05d_%d.cmd' % (rootname, epochs[i]), 'w') as file:
+            file.write(filedata)
+
+        p = subprocess.Popen(shlex.split("qsub mpa-pascal_%05d_%d.cmd" % (rootname, epochs[i])))
+
+if __name__ == "__main__":
+    write_submit(Nthreads, rootname, defaultfile)
+#write_submit(16, 'tardis_05602_1.yml', 5602)
