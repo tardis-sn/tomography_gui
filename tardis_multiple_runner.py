55ae3dd7cd0d1db22c4679ff658092de3e845b6d
Talytha Barbosa <talytha@mpa-garching.mpg.de>
2016-12-15 14:33:18 +0100

    Included option to run GUI at local machine.

--- tardis_multiple_runner.py
+++ tardis_multiple_runner.py
@@ -18,27 +18,53 @@ def main(rootname):
 
     return epochs
 
-def write_submit(Nthreads, rootname, defaultfile = 'mpa-pascal-default.cmd'):
+def write_submit(Nthreads, rootname, mode = 'local'):
 
     epochs = main(rootname)
 
-    for i in xrange(len(epochs)):
+    if mode == 'batch':
+        defaultfile = 'mpa-pascal-defaul.cmd'
+        for i in xrange(len(epochs)):
 
-        filename = 'tardis_%05d_%d.yml' %(rootname, epochs[i])
+            filename = 'tardis_%05d_%d.yml' %(rootname, epochs[i])
 
-        with open(defaultfile, 'r') as file:
-            filedata = file.read()
+            with open(defaultfile, 'r') as file:
+                 filedata = file.read()
 
-        filedata = filedata.replace('NTHREADS', str(Nthreads))
-        filedata = filedata.replace('executablefile', './run_tardis.py')
-        filedata = filedata.replace('filename', str(filename))
-        filedata = filedata.replace('rootname', str(rootname))
-        filedata = filedata.replace('epoch', str(epochs[i]))
+            filedata = filedata.replace('NTHREADS', str(Nthreads))
+            filedata = filedata.replace('executablefile', './run_tardis.py')
+            filedata = filedata.replace('filename', str(filename))
+            filedata = filedata.replace('rootname', str(rootname))
+            filedata = filedata.replace('epoch', str(epochs[i]))
 
-        with open('mpa-pascal_%05d_%d.cmd' % (rootname, epochs[i]), 'w') as file:
-            file.write(filedata)
+            with open('mpa-pascal_%05d_%d.cmd' % (rootname, epochs[i]), 'w') as file:
+                file.write(filedata)
 
-        p = subprocess.Popen(shlex.split("qsub mpa-pascal_%05d_%d.cmd" % (rootname, epochs[i])))
+            p = subprocess.Popen(shlex.split("qsub mpa-pascal_%05d_%d.cmd" % (rootname, epochs[i])))
 
-if __name__ == "__main__":
-    write_submit(Nthreads, rootname)
+    if mode == 'local':
+
+      for i in xrange(len(epochs)):
+            defaultfile = 'submit_local_default.sh'
+            filename = 'tardis_%05d_%d.yml' %(rootname, epochs[i])
+
+            with open(defaultfile, 'r') as file:
+                filedata = file.read()
+
+            filedata = filedata.replace('executablefile', './run_tardis.py')
+            filedata = filedata.replace('filename', str(filename))
+            filedata = filedata.replace('rootname', str(rootname))
+            filedata = filedata.replace('epoch', str(epochs[i]))
+
+            with open('submit_local_%05d_%d.sh' % (rootname, epochs[i]), 'w') as file:
+                file.write(filedata)
+
+            subprocess.Popen(shlex.split("chmod +x submit_local_%05d_%d.sh" % (rootname, epochs[i])))
+
+            with open("tardis_%05d_%d_err.log" %(rootname, epochs[i]), 'w+') as err:
+                subprocess.Popen(shlex.split("./submit_local_%05d_%d.sh" % (rootname, epochs[i])), stdout = err , stderr = err )
+
+#if __name__ == "__main__":
+#    write_submit(Nthreads, rootname, mode)
+
+write_submit(16,4)
